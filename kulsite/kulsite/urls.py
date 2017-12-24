135574edc0d41eb35f344d8e2e4e3858095f01e8
Nico Chera <nchera13@apu.edu>
2017-12-23 22:11:13 -0800

    Added basic urls

--- kulsite/kulsite/urls.py
+++ kulsite/kulsite/urls.py
@@ -14,8 +14,14 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
+    path('', RedirectView.as_view(url='home/')),
     path('admin/', admin.site.urls),
+    path('home/', include('home.urls')),
+    path('projects/', include('projects.urls')),
+    path('about/', include('about.urls'))
 ]
