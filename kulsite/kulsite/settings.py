27335fdb2248ae943135294b79ab31b5e1fcc1d3
Nico Chera <nchera13@apu.edu>
2017-12-23 21:53:53 -0800

    Updated hosts file

--- kulsite/kulsite/settings.py
+++ kulsite/kulsite/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = conf['prod']['secret']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [conf['host']['prod']]  # define more here in your config file
 
 
 # Application definition
