824f4752a11cfd8046b5213dbffe701591383e2d
Nico Chera <nchera13@apu.edu>
2017-12-27 00:40:50 -0800

    Homepage styling v1

--- kulsite/static/js/home.js
+++ kulsite/static/js/home.js
@@ -1,3 +1,48 @@
-$(document).on('load', function() {
+$(window).on('load', function() {
+var index = 0;
+var titles = [
+    "Dreamer",
+    "Developer",
+    "Writer",
+    "Pie Enthusiast",
+    "Pi Enthusiast",
+    "Raspberry Pi Enthusiast",
+    "Cryptocurrency Collector",
+    "Burrito Lover",
+    "Bibliophile",
+    "Riddler",
+    "Storyteller",
+    "Moviegoer",
+    "Scholar",
+];
+function updateSubtitle() {
+    var old_len = titles[index].length;
+    index = (index + 1) % titles.length;
+    var new_title = titles[index]
 
+    if (old_len <= new_title.length) {
+        $(".home-subtitle").animate({width: getTextWidth(new_title)},{duration: 100});
+    }
+    $(".home-subtitle").hide({
+        effect:'slide',
+        direction: 'left',
+        duration: 500,
+        complete: function() {
+            $(".home-subtitle").text(new_title);
+        }
+    });
+    $(".home-subtitle").show({effect: 'slide', direction:'right', duration:500,});
+    if (old_len > new_title.length) {
+        $(".home-subtitle").animate({width: getTextWidth(new_title)},{duration: 100});
+    }
+
+
+    var t = setTimeout(updateSubtitle, 3000);
+}
+updateSubtitle();
+
+function getTextWidth(text) {
+    $(".ruler").text(text);
+    return $(".ruler").width();
+}
 })
