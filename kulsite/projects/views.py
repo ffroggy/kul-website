f5b97e075bc4f7fdb7d04ac0ba61fb5dc4dd8af6
Nico Chera <nchera13@apu.edu>
2017-12-28 21:30:47 -0800

    Completed work on AJAX request and models

--- kulsite/projects/views.py
+++ kulsite/projects/views.py
@@ -1,5 +1,6 @@
 from kulsite.views import load_page
 from django.http import JsonResponse
+from .models import Skill
 
 
 def index(request):
@@ -10,5 +11,14 @@ def index(request):
 
 
 def ajax_skill(request, skill_id):
-    skill = {'skill': skill_id}
-    return JsonResponse(skill)
+    skill = Skill.objects.filter(s_id=skill_id).first()
+    if not skill:
+        return JsonResponse({'result': 'bad skill_id'})
+
+    skill_out = {
+        'status': 'success',
+        'name': skill.name,
+        'descr': skill.description,
+        'prof': skill.proficiency,
+    }
+    return JsonResponse(skill_out)
--- kulsite/static/js/projects.js
+++ kulsite/static/js/projects.js
@@ -1,4 +1,5 @@
 $(window).on('load', function() {
+
 $(".disp-picker").click(function(){
     $(".disp-picker").removeClass('active');
     $(this).addClass('active');
@@ -13,18 +14,34 @@ $.ajax({
     method: "GET",
     dataType: "json",
     url:"/projects/ajax/skill/"+$(this).attr('id'),
-    data: {
-        cool:"hi",
-    },
     error: function() {
         console.log("AJAX Error :(");
-        alert("AJAX ERROR!");
+        alert("AJAX error :'( Please try reloading the page.");
     },
     success: function(data) {
-        alert("Success!!");
         console.log(data);
+        show_skill(data);
     }
 });
+});
+
+function show_skill(data) {
+    if (data.status != "success") {
+        $("#skdet-header").text("Unknown");
+        $("#skdet-descr").text("The description for this skill doesn't exist yet! Sorry about that!");
+        $("#skdet-prof").text("0");
+    } else {
+        $("#skdet-header").text(data.name);
+        $("#skdet-descr").text(data.descr);
+        $("#skdet-prof").text(data.prof);
+    }
+    $("#skill-detail").modal('show');
+}
+
+$("#skdet-close").click(function() {
+    $("#skdet-header").text("");
+    $("#skdet-descr").text("");
+    $("#skdet-prof").text("");
 });
 
 });
